ef779d68c9d9d3e0fe1ddfb064228bfbc44fa267
Ryan Sherry <rsherrycomputers@gmail.com>
2018-05-06 09:52:15 -0700

    Created adapter and view holderas well as data model updates

--- app/src/main/java/teamtreehouse/com/stormy/ui/DailyForecastActivity.java
+++ app/src/main/java/teamtreehouse/com/stormy/ui/DailyForecastActivity.java
@@ -6,19 +6,19 @@ import android.os.Bundle;
 import android.widget.ArrayAdapter;
 
 import teamtreehouse.com.stormy.R;
+import teamtreehouse.com.stormy.adapters.DayAdapter;
+import teamtreehouse.com.stormy.weather.Day;
 
 public class DailyForecastActivity extends ListActivity  {
+
+    private Day[] mDays;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_daily_forecast);
 
-        String[] daysOfTheWeek = {"Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"};
-        ArrayAdapter<String> adapter = new ArrayAdapter<String>(this,
-                android.R.layout.simple_list_item_1,
-                daysOfTheWeek);
-        setListAdapter(adapter);
+        DayAdapter adapter = new DayAdapter(this,mDays);
 
     }
 }
--- app/src/main/java/teamtreehouse/com/stormy/weather/Day.java
+++ app/src/main/java/teamtreehouse/com/stormy/weather/Day.java
@@ -1,5 +1,9 @@
 package teamtreehouse.com.stormy.weather;
 
+import java.text.SimpleDateFormat;
+import java.util.Date;
+import java.util.TimeZone;
+
 public class Day {
     private long mTime;
     private String mSummary;
@@ -23,8 +27,8 @@ public class Day {
         mSummary = summary;
     }
 
-    public double getTemperatureMax() {
-        return mTemperatureMax;
+    public int getTemperatureMax() {
+        return (int) Math.round(mTemperatureMax);
     }
 
     public void setTemperatureMax(double temperatureMax) {
@@ -45,5 +49,16 @@ public class Day {
 
     public void setTimezone(String timezone) {
         mTimezone = timezone;
+    }
+
+    public int getIconId() {
+        return Forecast.getIconId(mIcon);
+    }
+
+    public String getDayOfTheWeek() {
+        SimpleDateFormat formatter = new SimpleDateFormat("EEEE");
+        formatter.setTimeZone(TimeZone.getTimeZone(mTimezone));
+        Date dateTime = new Date(mTime * 1000);
+        return formatter.format(dateTime);
     }
 }
